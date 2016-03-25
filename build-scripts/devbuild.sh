973f4b4317d2acc08acf2a316490f231139a487c
Joseph C Wang <joequant@gmail.com>
2016-03-25 21:10:44 +0800

    add ipfs

--- build-scripts/devbuild.sh
+++ build-scripts/devbuild.sh
@@ -8,3 +8,9 @@ cd $GOPATH/src/github.com/openblockchain/obc-peer/openchain/container
 go get .
 go test -run BuildImage_Peer -timeout 99999s
 
+cd ~
+export TARFILE=go-ipfs_v0.3.11_linux-amd64.tar.gz
+wget http://dist.ipfs.io/go-ipfs/v0.3.11/$TARFILE
+tar xzf $TARFILE
+sudo mv go-ipfs/ipfs /opt/go/bin/ipfs
+rm -rf $TARFILE go-ipfs
