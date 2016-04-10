20c054457fe2cf16d2b0cc9845c7dc42c807c30f
Joseph C Wang <joequant@gmail.com>
2016-04-10 12:54:27 +0800

    fix devbuild

--- build-scripts/devbuild.sh
+++ build-scripts/devbuild.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-go get github.com/joequant/openblockchain/examples/map
-cd $GOPATH/src/github.com/joequant/openblockchain/examples/map
-go build
 cd /hyperledger/core/container
 go test -run BuildImage_Peer -timeout 99999s
 
 cd ~
 export TARFILE=go-ipfs_v0.3.11_linux-amd64.tar.gz
+rm -f $TARFILE.*
 wget http://dist.ipfs.io/go-ipfs/v0.3.11/$TARFILE
 tar xzf $TARFILE
 sudo mv go-ipfs/ipfs /opt/go/bin/ipfs
