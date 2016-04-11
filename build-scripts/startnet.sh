192c02300730cc9817e217d191210af3e8075413
Joseph C Wang <joequant@gmail.com>
2016-04-11 10:22:31 +0800

    fix up startnet

--- build-scripts/startnet.sh
+++ build-scripts/startnet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MYDIR=`pwd`
-docker run --rm -t -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true -e CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 hyperledger-peer ./peer peer --logging-level=debug >& $MYDIR/vp1.log &
+docker run --rm -t -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true hyperledger-peer ./peer peer --logging-level=debug >& $MYDIR/vp1.log &
 docker run --rm -t -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e CORE_PEER_ID=vp2 -e CORE_PEER_ADDRESSAUTODETECT=true -e CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 hyperledger-peer ./peer peer --logging-level=debug >& $MYDIR/vp2.log &
 cat
 
